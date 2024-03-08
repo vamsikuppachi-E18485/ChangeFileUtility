d7a34dc13c79a034279a00bab6daceb71aeafd4d
Vamsi Kuppachi <VKuppachi@aflac.com>
2024-03-08 11:52:41 -0500

    Initial Update
    Initial Update

---
+++ ChangeFileUtility/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace ChangeFileUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "NewFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OldFile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(634, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(195, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(634, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BlueSkyCSV",
+            "StandardCSV",
+            "StandardTXT"});
+            this.comboBox1.Location = new System.Drawing.Point(195, 200);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FileFormat";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(378, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "CreateDeltaFile";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(375, 314);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(72, 17);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DeltaFilePath";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(375, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 380);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChangeFileUtility/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeFileUtility
+{
+    public partial class Form1 : Form
+    {
+        public string _OldFileName = string.Empty;
+        public string _NewFileName = string.Empty;
+        public string _FileFormat = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            label3.Visible = false;
+            linkLabel1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            _NewFileName = openFileDialog1.FileName;
+            textBox1.Text = _NewFileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            _OldFileName = openFileDialog1.FileName;
+            textBox2.Text = _OldFileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_NewFileName.Length == 0)
+            {
+                label3.Visible = true;
+                label3.Text = "Missing New File";
+                return;
+            }
+            else if (_OldFileName.Length == 0)
+            {
+                label3.Visible = true;
+                label3.Text = "Missing old File";
+                return;
+            }
+            else if (_FileFormat.Length == 0)
+            {
+                label3.Visible = true;
+                label3.Text = "Missing old File";
+                return;
+            }
+
+            FileCompare(_FileFormat.ToUpper());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _FileFormat = comboBox1.SelectedItem.ToString();
+        }
+        public void FileCompare(string _FileFormat)
+        {
+            try
+            {
+                string sNewerFilePath = _NewFileName;
+                string sOlderFilePath = _OldFileName;
+                string[] _newFile = File.ReadAllLines(sNewerFilePath);
+                string[] _oldFile = File.ReadAllLines(sOlderFilePath);
+                List<string> _DeltaFile = new List<string>();
+                List<string> _AddedEmplList = new List<string>();
+
+                List<string> _newFileUpper = _newFile.ToList().Select(x => x.ToUpper()).ToList();
+                List<string> _oldFileUpper = _oldFile.ToList().Select(x => x.ToUpper()).ToList();
+
+                //var deltaData = _newFileUpper.ToArray().Except(_oldFileUpper.ToArray());
+                var deltaData = _newFileUpper.Union(_oldFileUpper).Except(_newFileUpper.Intersect(_oldFileUpper));
+                //foreach (var _line in _newFile)
+                //{
+                //    if (!_oldFile.ToArray().Where(x=> x == _line.ToString()).Any())
+                //    {
+                //        string EmplID = ExtractEmplID(_line, _FileFormat);
+                //        foreach (var MatchedLine in _newFile.ToList().Where(x => x.Contains(EmplID)))
+                //        {
+                //            if (MatchedLine.IndexOf(EmplID) < 15)
+                //            { 
+                //                if (!_AddedEmplList.Contains(EmplID))
+                //                {
+                //                    _DeltaFile.Add(MatchedLine);
+                //                }
+                //            }
+                //        }
+                //        _AddedEmplList.Add(EmplID);
+                //    }
+                //}
+
+                foreach (var _line in deltaData)
+                {
+                    string EmplID = ExtractEmplID(_line, _FileFormat);
+                    foreach (var MatchedLine in _newFile.ToList().Where(x => x.Contains(EmplID)))
+                    {
+                        if (MatchedLine.IndexOf(EmplID) < 15)
+                        {
+                            if (!_AddedEmplList.Contains(EmplID))
+                            {
+                                _DeltaFile.Add(MatchedLine);
+                            }
+                        }
+                    }
+                    _AddedEmplList.Add(EmplID);
+                }
+                string _DeltaFileName = DeltaFileName(_NewFileName);
+                File.WriteAllLines(_DeltaFileName, _DeltaFile.ToArray());
+                linkLabel1.Visible = true;
+                linkLabel1.Text = _DeltaFileName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public string ExtractEmplID(string _lineVal, string _FileFormat)
+        {
+            if (_FileFormat == "STANDARDTXT")
+            {
+                return StandardTxtReturn(_lineVal);
+            }
+            else if (_FileFormat == "STANDARDCSV")
+            {
+                return StandardCSVReturn(_lineVal);
+            }
+            else if (_FileFormat == "BLUESKYCSV")
+            {
+                return BlueSkyCSVReturn(_lineVal);
+            }
+            return string.Empty;
+        }
+        public string DeltaFileName(string OriginalFileName)
+        {
+             string _NewFileName = OriginalFileName.Split('\\').LastOrDefault().ToString();
+            string _Extension = OriginalFileName.Substring(OriginalFileName.IndexOf("."));
+            string _AppendedName = "_Modified" + DateTime.Now.ToFileTime();
+            _NewFileName = _NewFileName.Replace(_Extension, _AppendedName) + _Extension;
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var FinalFilePath = Path.Combine(path, _NewFileName);
+            return FinalFilePath;
+        }
+        public string StandardTxtReturn(string _lineVal)
+        {
+            var EmplID = string.Empty;
+            if (_lineVal.StartsWith("EMPL"))
+            {
+                EmplID = _lineVal.Substring(8, 9);
+            }
+            else if (_lineVal.StartsWith("DPDT") || _lineVal.StartsWith("SPSE") || _lineVal.StartsWith("BENE"))
+            {
+                EmplID = _lineVal.Substring(5, 9);
+            }
+            else
+            {
+                EmplID = _lineVal.Substring(9, 9);
+            }
+            return EmplID;
+        }
+        public string StandardCSVReturn(string _lineVal)
+        {
+            var EmplID = string.Empty;
+            if (_lineVal.Contains("DPDT") || _lineVal.Contains("SPSE"))
+            {
+                EmplID = _lineVal.Split(',')[2].ToString();
+            }
+            else
+            {
+                EmplID = _lineVal.Split(',')[3].ToString();
+            }
+            return EmplID;
+        }
+        public string BlueSkyCSVReturn(string _lineVal)
+        {
+            var EmplID = _lineVal.Split(',')[2].ToString();
+            if (EmplID.Length == 0)
+            {
+                EmplID = _lineVal.Split(',')[3].ToString();
+            }
+            return EmplID;
+        }
+    }
+
+}
+
